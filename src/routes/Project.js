3716e95f6555c112210bb6940c08c75e7f63be5f
muratozbk <slipstream11@yandex.com>
2023-09-29 21:43:34 +0300

    work cards

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroComp from '../components/HeroComp'
 import PricingCard from '../components/PricingCard'
+import WorkCard from '../components/WorkCard'
 
 function Project() {
     return (
@@ -10,7 +11,7 @@ function Project() {
             <Navbar />
             <HeroComp heading="PROJECTS"
                 text='Some of my recent works' />
-
+            <WorkCard />
             <PricingCard />
             <Footer />
         </div>
